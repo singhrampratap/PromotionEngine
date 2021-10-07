4405fa419a3f7b57880b33232b3506053d8b999f
singhrampratap <singh.ram25327@gmail.com>
2021-10-07 21:19:00 +0530

    Create Rule.java

---
+++ promotionEng-data-module/src/main/java/org/learning/promo/data/Rule.java
@@ -0,0 +1,12 @@
+package org.learning.promo.data;
+
+import lombok.Data;
+
+@Data
+public class Rule {
+
+    int ruleId;
+    String desc;
+    PromotionType promotionType;
+    BaseCondition baseCondition;
+}
